93faa7d8d580763e7bc1dc63fb05a2db209d27cf
noby <n.fujioka@abacusmaths.info>
2016-10-20 23:06:59 +0100

    adding map marker functionality. WIP.

--- angularApp.js
+++ angularApp.js
@@ -3,20 +3,53 @@ var app = angular.module('ecoAngular1App',[]);
 app.factory('myDetails', ['$http', function($http){
 
   var o = {
+    myDetails:{
     "latitude": "55.378051",
     "vehicleMake": "",
     "vehicleModel": "",
     "vehicleSpec": "",
     "longitude": "-3.435973"
+    }
   };
 
   o.set_car = function(selectedCar){
     console.log("hello, in factory! selectedCar:", selectedCar);
-    // o.vehicleMake = selectedCar.vehicleMake;
-    // o.vehicleModel = selectedCar.vehicleModel;
-    // o.vehicleSpec = selectedCar.vehicleSpec;
+    o.myDetails.vehicleMake = selectedCar.vehicleMake;
+    o.myDetails.vehicleModel = selectedCar.vehicleModel;
+    o.myDetails.vehicleSpec = selectedCar.vehicleSpec;
   };
 
+  o.getMyCoords = function(){
+    var instructionEl = document.getElementById("instruction");
+    if (navigator.geolocation) {
+      instructionEl.innerHTML = "<span class='animated fadeIn'>Finding your location...</span>";
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        instructionEl.innerHTML = "Geolocation is not supported by this browser.";
+    }
+    function showPosition(position){
+      console.log("latlng:", position.coords.latitude+ " " + position.coords.longitude );
+      o.myDetails.latitude = position.coords.latitude;
+      o.myDetails.longitude = position.coords.longitude;
+      instructionEl.innerHTML = "<span class='animated fadeIn'>Got your location.</span>";
+    }
+    function showError(error) {
+        switch(error.code) {
+          case error.PERMISSION_DENIED:
+              instructionEl.innerHTML = "User denied the request for Geolocation.";
+              break;
+          case error.POSITION_UNAVAILABLE:
+              instructionEl.innerHTML = "Location information is unavailable.";
+              break;
+          case error.TIMEOUT:
+              instructionEl.innerHTML = "The request to get user location timed out.";
+              break;
+          case error.UNKNOWN_ERROR:
+              instructionEl.innerHTML = "An unknown error occurred.";
+              break;
+        }
+    }
+  };
   return o;
 }]);
 
@@ -48,23 +81,28 @@ app.factory('chargingStations', ['$http', function($http){
   return o;
 }]);
 
-app.service('Map', [function($q) {
-
-    this.init = function() {
-        var options = {
-            center: new google.maps.LatLng(55.378051, -3.435973),
-            zoom: 10,
-            disableDefaultUI: true
-        };
-        this.map = new google.maps.Map(
-            document.getElementById("map"), options
-        );
-        this.places = new google.maps.places.PlacesService(this.map);
+app.service('myMap', [function($q) {
+    var o = {};
+    o.init = function() {
+      var centerOfUK = new google.maps.LatLng(53.4,-1.45);
+      var mapCanvas = document.getElementById("map");
+      var options = {
+          center: centerOfUK,
+          zoom: 6,
+          disableDefaultUI: true
+      };
+      o.map = new google.maps.Map(
+          document.getElementById("map"), options
+      );
+      o.directionsService = new google.maps.DirectionsService;
+      o.directionsDisplay = new google.maps.DirectionsRenderer;
+      o.places = new google.maps.places.PlacesService(o.map);
+      o.map = new google.maps.Map(mapCanvas, options);
     };
 
-    this.search = function(str) {
+    o.search = function(str) {
         var d = $q.defer();
-        this.places.textSearch({query: str}, function(results, status) {
+        o.places.textSearch({query: str}, function(results, status) {
             if (status == 'OK') {
                 d.resolve(results[0]);
             }
@@ -73,54 +111,54 @@ app.service('Map', [function($q) {
         return d.promise;
     };
 
-    this.addMarker = function(res) {
-        if(this.marker) this.marker.setMap(null);
-        this.marker = new google.maps.Marker({
-            map: this.map,
+    o.addMarker = function(res) {
+        if(o.marker) o.marker.setMap(null);
+        o.marker = new google.maps.Marker({
+            map: o.map,
             position: res.geometry.location,
             animation: google.maps.Animation.DROP
         });
         this.map.setCenter(res.geometry.location);
     };
-
+  return o;
 }]);
 
-app.controller('newPlaceCtrl', [
-    'Map',
-    function(Map) {
-      var self= this;
-      console.log("hello");
-      self.place = {};
-      self.search = function() {
-          self.apiError = false;
-          Map.search(self.searchPlace)
-          .then(
-              function(res) { // success
-                  Map.addMarker(res);
-                  self.place.name = res.name;
-                  self.place.lat = res.geometry.location.lat();
-                  self.place.lng = res.geometry.location.lng();
-              },
-              function(status) { // error
-                  self.apiError = true;
-                  self.apiStatus = status;
-              }
-          );
-      };
-
-    self.send = function() {
-        alert(self.place.name + ' : ' + self.place.lat + ', ' + self.place.lng);
-    };
-
-    Map.init();
-}]);
+// app.controller('newPlaceCtrl', [
+//     'myMap',
+//     function(myMap) {
+//       myMap.init();
+//       var self= this;
+//       console.log("hello");
+//       self.place = {};
+//       self.search = function() {
+//           self.apiError = false;
+//           myMap.search(self.searchPlace)
+//           .then(
+//               function(res) { // success
+//                   myMap.addMarker(res);
+//                   self.place.name = res.name;
+//                   self.place.lat = res.geometry.location.lat();
+//                   self.place.lng = res.geometry.location.lng();
+//               },
+//               function(status) { // error
+//                   self.apiError = true;
+//                   self.apiStatus = status;
+//               }
+//           );
+//       };
+//
+//     self.send = function() {
+//         alert(self.place.name + ' : ' + self.place.lat + ', ' + self.place.lng);
+//     };
+//
+// }]);
 
 
 app.controller('mainCtrl',[
-    'chargingStations', 'myDetails',
-    function(chargingStations, myDetails ){
+    'chargingStations', 'myDetails', 'myMap',
+    function(chargingStations, myDetails, myMap ){
       var self = this;
-
+      myMap.init();
       self.listOfCars = [
         {vehicleMake: "Audi", vehicleModel: "A3 e-tron", vehicleSpec: "(2014-)"},
         {vehicleMake: "Audi", vehicleModel: "Q7 e-Tron", vehicleSpec: "(2015-)"},
@@ -131,13 +169,34 @@ app.controller('mainCtrl',[
         {vehicleMake: "Ford", vehicleModel: "Focus EV", vehicleSpec: "(2014-)"},
         {vehicleMake: "Ford", vehicleModel: "Fusion Energi", vehicleSpec: "(2015-)"}
       ];
-
       self.select_car = function(selectedCar){
         console.log("myDetails:", myDetails);
         console.log("hello, in controller! selectedCar:", selectedCar);
         myDetails.set_car(selectedCar);
-        chargingStations.getAll(myDetails);
+        myDetails.getMyCoords();
+        chargingStations.getAll(myDetails.myDetails);
         self.list = chargingStations.chargingStations;
+      };
+
+      self.markers = [];
+
+      var infoWindow = new google.maps.InfoWindow();
+      var createMarker = function(info) {
+        var marker = new google.maps.Marker({
+          map: myMap.map,
+          position: new google.maps.LatLng(info.lat, info.long),
+          title: info.place
+        });
+        marker.content = '<div class="infoWindowContent">' + info.desc + '<br />' + info.lat + ' E,' + info.long +  ' N, </div>';
+
+        google.maps.event.addListener(marker, 'click', function(){
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' +
+              marker.content);
+            infoWindow.open(myMap.map, marker);
+        });
+
+        self.markers.push(marker);
+
       };
 
       // chargingStations.getAll(myDetails);
